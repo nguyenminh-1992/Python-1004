ce3d88e1205cae4169c0166966533c03896a00a6
nguyenminh-1992 <nguyenleminh.paris13@gmail.com>
2024-05-29 20:08:05 +0700

    chuongtrinhqlhv

--- Buoi13/chuongtrinhqlhv.py
+++ Buoi13/chuongtrinhqlhv.py
@@ -6,4 +6,11 @@
 #D: Delete   (Xóa học viên)
 
 #Tkinter
-#Bấm phím số: 
+#Bấm phím số:
+#Có sự lựa chọn, chương trình chạy mãi
+print("1. Thêm học viên")
+print("2. Hiển thị danh sách học viên")
+print("3. Sửa thông tin học viên")
+print("4. Xóa học viên")
+print("0. Thoát")
+nhap = int(input("Nhập chức năng muốn chọn: "))

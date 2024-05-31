3cf10d13171e8c1268c6173ea1ffe2a3b28dc478
nguyenminh-1992 <nguyenleminh.paris13@gmail.com>
2024-05-31 20:42:17 +0700

    dinh nghia class chung

---
+++ Buoi14/classdinhnghia.py
@@ -0,0 +1,10 @@
+#Dinh nghia chung cho tat cac ca lop Python
+
+class classchung():
+    def __init__(self,ten,tuoi,quequan,lop,tienganh,tinhoc):
+        self.ten = ten
+        self.tuoi = tuoi
+        self.quequan = quequan
+        self.lop = lop
+        self.tienganh = tienganh
+        self.tinhoc = tinhoc 

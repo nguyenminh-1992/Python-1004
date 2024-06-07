2d006d5bf2bf9b000bb941b5a200f48bc5732f8a
nguyenminh-1992 <nguyenleminh.paris13@gmail.com>
2024-06-07 20:34:06 +0700

    them hoc vien - stt

--- Buoi16/class1004.py
+++ Buoi16/class1004.py
@@ -4,6 +4,7 @@ class class1004:
     listclass1004 = [] #hocvien()
 
     def themhocvien(self):
+        stt = self.listclass1004.__len__() + 1
         ten = input("Nhap ten: ")
         tuoi = int(input("Nhap tuoi: "))
         quequan = input("Nhap que quan: ")
@@ -12,6 +13,7 @@ class class1004:
         tinhoc = int(input("Nhap diem tin hoc: "))
         diemtrungbinh = float((tinhoc+tienganh)/2)
         hocvien = classchung(ten,tuoi,quequan,lop,tienganh,tinhoc)
+        hocvien.stt = stt
         if diemtrungbinh>5:
             hocvien.xeploai = "PASS"
         else:
@@ -19,12 +21,14 @@ class class1004:
         self.listclass1004.append(hocvien)
 
     def hienthihocvien(self):
-        print("{:<15}{:<10}{:<15}{:<10}{:<15}{:<15}{:<15}".format("Ten","Tuoi","Quequan","Lop","TiengAnh","Tinhoc","Xeploai"))
+        print("{:<15}{:<15}{:<10}{:<15}{:<10}{:<15}{:<15}{:<15}".format("STT","Ten","Tuoi","Quequan","Lop","TiengAnh","Tinhoc","Xeploai"))
         for i in self.listclass1004:
-            print("{:<15}{:<10}{:<15}{:<10}{:<15}{:<15}{:<15}".format(i.ten,i.tuoi,i.quequan,i.lop,i.tienganh,i.tinhoc,i.xeploai))
+            print("{:<15}{:<15}{:<10}{:<15}{:<10}{:<15}{:<15}{:<15}".format(i.stt,i.ten,i.tuoi,i.quequan,i.lop,i.tienganh,i.tinhoc,i.xeploai))
     
     #tim kiem:
 
 #test = class1004()
 #test.themhocvien()
 #test.hienthihocvien()
+
+

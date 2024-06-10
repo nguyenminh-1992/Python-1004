d659f8d958733e53a93d72aea5171f914d4e08fe
nguyenminh-1992 <nguyenleminh.paris13@gmail.com>
2024-06-10 21:22:33 +0700

    Bai tap buoi 17

---
+++ Buoi17/baitap.py
@@ -0,0 +1,16 @@
+# Xây dựng chương trình Quản lý nhân viên
+# Nhân viên (tên, tuổi, quê quán, số ngày công)  - ĐỊNH NGHĨA
+# Nhân viên (cán bộ - CB, trưởng phòng - TP, giám đốc - GD)
+# Yêu cầu:
+# Khi thêm nhân viên, chức năng chọn loại nhân viên
+# Mã nhân viên: NVx  (trong đó x là thứ tự của loại nhân viên)
+#Ví dụ: Thêm nv đầu tiên là CB thì mã nhân viên: CB1
+#Thêm nv thứ 2 là TP thì mã nhân viên: TP1
+#Thêm nv thứ 3 là CB thì mã nhân viên: CB2
+
+#Tính lương theo số ngày công (CB - 100.000, TP - 200.000, GD - 300.000) hiển thị lương
+# ở mục Hiển thị danh sách nhân viên
+# Mã nv  -   Tên   - Tuổi    - Quê quán    - Số ngày công - Lương (VND)
+
+
+#Hoàn thiện các chức năng CRUD (Thêm, sửa, xóa, hiển thị nhân viên)
